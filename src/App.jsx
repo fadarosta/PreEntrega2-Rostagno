bfbdf5c5c0d3689ed2f7b182e45d9476080f4061
Fabio Rostagno <fadarosta@gmail.com>
2025-03-03 21:00:40 -0300

    base

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ItemListContainer from './Components/itemListContainer/itemListContainer'
+import NavBar from './Components/NavBar/NavBar'
+function App () {
+  return (
+    <>
+    <NavBar/>
+    <ItemListContainer greeting={'Yoguini,'}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/itemListContainer/itemListContainer.jsx
@@ -0,0 +1,9 @@
+
+
+const ItemListContainer = ({ greeting }) => {
+    return (
+        <h4>Bienvenido {greeting} a ANAN7A Multiespacio</h4>
+    )
+}
+
+export default ItemListContainer
